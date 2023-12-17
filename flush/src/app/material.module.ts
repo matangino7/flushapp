ae37c6b5b405cb8680085a1fec027fc49cf256ba
Matan Gino <matangino@Matans-MacBook-Pro.local>
2023-12-17 14:50:02 +0200

    navbar

--- flush/src/app/material.module.ts
+++ flush/src/app/material.module.ts
@@ -3,7 +3,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field' 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   imports: [
@@ -13,6 +17,10 @@ import { MatButtonModule } from '@angular/material/button'
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
   ],
   exports: [
     FormsModule,
@@ -21,6 +29,10 @@ import { MatButtonModule } from '@angular/material/button'
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
   ],
 })
 export class MyMaterialModule { }
